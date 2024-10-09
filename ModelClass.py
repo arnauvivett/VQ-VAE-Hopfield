aaf4b3b700d43205e02a84c9c8e7d742f671b93f
arnauvivett <86897029+arnauvivett@users.noreply.github.com>
2024-10-09 12:25:12 +0200

    Create ModelClass.py

---
+++ ModelClass.py
@@ -0,0 +1,32 @@
+class Model(nn.Module):
+    def __init__(self, num_hiddens, num_residual_layers, num_residual_hiddens,
+                 num_embeddings, embedding_dim, commitment_cost, decay=0):
+        
+        super(Model, self).__init__()
+
+        self._encoder = Encoder(1, num_hiddens,
+                                num_residual_layers,
+                                num_residual_hiddens)
+        self._pre_vq_conv = nn.Conv2d(in_channels=num_hiddens,
+                                      out_channels=embedding_dim,
+                                      kernel_size=1,
+                                      stride=1)
+        if decay > 0.0:
+            self._vq_vae = VectorQuantizerEMA(num_embeddings, embedding_dim,
+                                              commitment_cost, decay)
+            print("softmax_quantizer")
+        else:
+            self._vq_vae = VectorQuantizer(num_embeddings, embedding_dim,
+                                           commitment_cost)
+        self._decoder = Decoder(embedding_dim,
+                                num_hiddens,
+                                num_residual_layers,
+                                num_residual_hiddens)
+
+    def forward(self, x,beta,lambd):
+        z = self._encoder(x)
+        z = self._pre_vq_conv(z)
+        loss, quantized, perplexity, encodings = self._vq_vae(z,beta,lambd)
+        x_recon = self._decoder(quantized)
+
+        return loss, x_recon, perplexity, encodings
